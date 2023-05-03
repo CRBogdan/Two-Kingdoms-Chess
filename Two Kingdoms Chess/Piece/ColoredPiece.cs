085d1db55849b31ab0ecd8618e06bab0ec57eb3e
CRBogdan <90236649+CRBogdan@users.noreply.github.com>
2023-05-03 07:49:00 -0300

    get moves refact (#8)

--- Two Kingdoms Chess/Piece/ColoredPiece.cs
+++ Two Kingdoms Chess/Piece/ColoredPiece.cs
@@ -9,13 +9,14 @@ namespace Two_Kingdoms_Chess
     public abstract class ColoredPiece
     {
         public Piece piece;
+        public String color;
 
         public abstract String getPieceColor();
 
 
         public List<Move> getMoves(ColoredPiece[,] table)
         {
-            return piece.getPossibleMoves(table);
+            return piece.getPossibleMoves(table, color);
         }
     }
 }
--- Two Kingdoms Chess/Piece/Piece.cs
+++ Two Kingdoms Chess/Piece/Piece.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.Devices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,10 +17,25 @@ namespace Two_Kingdoms_Chess
             this.pieceName = pieceName;
         }
 
-        public abstract List<Move> getPossibleMoves(ColoredPiece[,] table);
+        public abstract List<Move> getPossibleMoves(ColoredPiece[,] table, String color);
         public void setPosition(Position position)
         {
             this.position = position;
+        }
+
+        protected Move getMove(ColoredPiece[,] table, Position position, String color)
+        {
+            if (table[position.x, position.y] != null)
+            {
+                if(color != table[position.x, position.y].color)
+                    return new OffensiveMove(this, new Position(position.x, position.y));
+            }
+            else
+            {
+                return new NormalMove(this, new Position(position.x, position.y));
+            }
+
+            return null;
         }
     }
 }
