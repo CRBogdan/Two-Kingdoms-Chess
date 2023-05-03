5fd635a818c39a2816c8184519f5b387c5dcb522
CRBogdan <90236649+CRBogdan@users.noreply.github.com>
2023-05-03 10:57:54 +0300

    MoveNode

---
+++ Two Kingdoms Chess/Piece/MoveNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Kingdoms_Chess
+{
+    public class MoveNode
+    {
+        public List<MoveNode> moveNodes;
+        public List<Move> moves;
+
+        public MoveNode(List<MoveNode> moveTrees, List<Move> moves)
+        {
+            this.moveNodes = moveTrees;
+            this.moves = moves;
+        }
+
+        public List<Move> getMovesList()
+        {
+            List<Move> movesToReturn = new List<Move>();
+            if (moves != null)
+                movesToReturn.AddRange(moves);
+
+            if (moveNodes == null)
+                return movesToReturn;
+
+            foreach (var m in moveNodes)
+            {
+                var mo = m.getMovesList();
+                movesToReturn.AddRange(mo);
+            }
+
+            return movesToReturn;
+        }
+    }
+}
