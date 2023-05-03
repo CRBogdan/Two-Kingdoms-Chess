91704646eea589baa668e549f5f5d9525115d5cb
CRBogdan <90236649+CRBogdan@users.noreply.github.com>
2023-05-03 12:23:56 +0300

    refact king

--- Two Kingdoms Chess/Piece/ConcretPiece/King.cs
+++ Two Kingdoms Chess/Piece/ConcretPiece/King.cs
@@ -45,39 +45,55 @@ namespace Two_Kingdoms_Chess
                     moves.Add(new NormalMove(this, new Position(position.x - 1, position.y)));
                 }
 
-            //if (position.y - 1 >= 0)
-            //    moves.moveNodes.Add(new MoveNode(null, new List<Move>()
-            //    {
-            //        new OffensiveMove(this, new Position(position.x, position.y - 1)),
-            //        new NormalMove(this, new Position(position.x, position.y - 1)),
-            //    }));
+            if (position.y - 1 >= 0)
+                if (table[position.x, position.y - 1] != null)
+                {
+                    moves.Add(new OffensiveMove(this, new Position(position.x, position.y - 1)));
+                }
+                else
+                {
+                    moves.Add(new NormalMove(this, new Position(position.x, position.y - 1)));
+                }
 
-            //if (position.x + 1 < 10 && position.y + 1 < 10)
-            //    moves.moveNodes.Add(new MoveNode(null, new List<Move>()
-            //    {
-            //        new OffensiveMove(this, new Position(position.x + 1, position.y + 1)),
-            //        new NormalMove(this, new Position(position.x + 1, position.y + 1)),
-            //    }));
+            if (position.x + 1 < 10 && position.y + 1 < 10)
+                if (table[position.x + 1, position.y + 1] != null)
+                {
+                    moves.Add(new OffensiveMove(this, new Position(position.x + 1, position.y + 1)));
+                }
+                else
+                {
+                    moves.Add(new NormalMove(this, new Position(position.x + 1, position.y + 1)));
+                }
 
-            //if (position.x - 1 >= 0 && position.y - 1 >= 0)
-            //    moves.moveNodes.Add(new MoveNode(null, new List<Move>()
-            //    {
-            //        new OffensiveMove(this, new Position(position.x - 1, position.y - 1))
-            //    }));
+            if (position.x - 1 >= 0 && position.y - 1 >= 0)
+                if (table[position.x - 1, position.y - 1] != null)
+                {
+                    moves.Add(new OffensiveMove(this, new Position(position.x - 1, position.y - 1)));
+                }
+                else
+                {
+                    moves.Add(new NormalMove(this, new Position(position.x - 1, position.y - 1)));
+                }
 
-            //if (position.x - 1 >= 0 && position.y + 1 < 10)
-            //    moves.moveNodes.Add(new MoveNode(null, new List<Move>()
-            //    {
-            //        new OffensiveMove(this, new Position(position.x - 1, position.y + 1)),
-            //        new NormalMove(this, new Position(position.x - 1, position.y + 1)),
-            //    }));
+            if (position.x - 1 >= 0 && position.y + 1 < 10)
+                if (table[position.x - 1, position.y + 1] != null)
+                {
+                    moves.Add(new OffensiveMove(this, new Position(position.x - 1, position.y + 1)));
+                }
+                else
+                {
+                    moves.Add(new NormalMove(this, new Position(position.x - 1, position.y + 1)));
+                }
 
-            //if (position.x + 1 < 10 && position.y - 1 >= 0)
-            //    moves.moveNodes.Add(new MoveNode(null, new List<Move>()
-            //    {
-            //        new OffensiveMove(this, new Position(position.x + 1, position.y - 1)),
-            //        new NormalMove(this, new Position(position.x + 1, position.y - 1)),
-            //    }));
+            if (position.x + 1 < 10 && position.y - 1 >= 0)
+                if (table[position.x + 1, position.y - 1] != null)
+                {
+                    moves.Add(new OffensiveMove(this, new Position(position.x + 1, position.y - 1)));
+                }
+                else
+                {
+                    moves.Add(new NormalMove(this, new Position(position.x + 1, position.y - 1)));
+                }
 
             return moves;
         }
