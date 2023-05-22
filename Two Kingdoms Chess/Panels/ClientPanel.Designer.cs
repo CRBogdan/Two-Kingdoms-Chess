df05da2b378c0139e1a6cf7bcbf93d3f0af22f4e
CRBogdan <90236649+CRBogdan@users.noreply.github.com>
2023-05-22 14:37:03 +0300

    Implemented TCP Client

--- Two Kingdoms Chess/Panels/ClientPanel.Designer.cs
+++ Two Kingdoms Chess/Panels/ClientPanel.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Two_Kingdoms_Chess.Panels
+﻿namespace Two_Kingdoms_Chess
 {
     partial class ClientPanel
     {
@@ -30,14 +30,12 @@
         {
             ipAddress = new TextBox();
             label1 = new Label();
-            label2 = new Label();
-            port = new TextBox();
             connect = new Button();
             SuspendLayout();
             // 
             // ipAddress
             // 
-            ipAddress.Location = new Point(148, 328);
+            ipAddress.Location = new Point(3, 23);
             ipAddress.Name = "ipAddress";
             ipAddress.Size = new Size(340, 27);
             ipAddress.TabIndex = 0;
@@ -45,47 +43,30 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(296, 305);
+            label1.Location = new Point(3, 0);
             label1.Name = "label1";
             label1.Size = new Size(78, 20);
             label1.TabIndex = 1;
             label1.Text = "IP Address";
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(296, 358);
-            label2.Name = "label2";
-            label2.Size = new Size(35, 20);
-            label2.TabIndex = 3;
-            label2.Text = "Port";
-            // 
-            // port
-            // 
-            port.Location = new Point(148, 381);
-            port.Name = "port";
-            port.Size = new Size(340, 27);
-            port.TabIndex = 2;
-            // 
             // connect
             // 
-            connect.Location = new Point(275, 414);
+            connect.Location = new Point(3, 56);
             connect.Name = "connect";
             connect.Size = new Size(94, 29);
             connect.TabIndex = 4;
             connect.Text = "Connect";
             connect.UseVisualStyleBackColor = true;
+            connect.Click += connect_Click;
             // 
-            // Client
+            // ClientPanel
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(connect);
-            Controls.Add(label2);
-            Controls.Add(port);
             Controls.Add(label1);
             Controls.Add(ipAddress);
-            Name = "Client";
+            Name = "ClientPanel";
             Size = new Size(700, 700);
             ResumeLayout(false);
             PerformLayout();
@@ -95,8 +76,6 @@
 
         private TextBox ipAddress;
         private Label label1;
-        private Label label2;
-        private TextBox port;
         private Button connect;
     }
 }
--- Two Kingdoms Chess/Player/Server.cs
+++ Two Kingdoms Chess/Player/Server.cs
@@ -1,10 +1,45 @@
-﻿namespace Two_Kingdoms_Chess
+﻿using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Two_Kingdoms_Chess
 {
     public class Server : Player
     {
+        private TcpClient server;
+
+        public void setServer(TcpClient server)
+        { 
+            this.server = server; 
+        }
+
         public override void onPlayerMove(Move move)
         {
-            this.gameChangeHandle(move);
+            NetworkStream stream = server.GetStream();
+
+            string moveJSON = JsonConvert.SerializeObject(new MoveDTO(move));
+
+            byte[] data = Encoding.UTF8.GetBytes(moveJSON);
+
+            stream.WriteAsync(data);
+
+            waitForPlayer();
+        }
+
+        public async void waitForPlayer()
+        {
+            NetworkStream stream = server.GetStream();
+            byte[] buffer = new byte[4096];
+
+            await stream.ReadAsync(buffer);
+
+            string json = Encoding.UTF8.GetString(buffer);
+
+            MoveDTO moveDTO = JsonConvert.DeserializeObject<MoveDTO>(json);
+
+            Move move = makeMove(game.gameTable[moveDTO.oldPositionX, moveDTO.oldPositionY], new Position(moveDTO.newPositionX, moveDTO.newPositionY));
+
+            game.movePiece(this, move);
         }
     }
 }
